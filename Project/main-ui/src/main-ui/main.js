54450fe8866d3b842cafe5bfb5f3b2097db248d4
Maksatt <45325024+Maksatt@users.noreply.github.com>
2021-01-23 00:27:38 +0600

    starting

---
+++ Project/main-ui/src/main-ui/main.js
@@ -0,0 +1,11 @@
+import React from 'React'
+import classes from './main.module.scss'
+
+const Main  = props => {
+    return(
+        <div className={classes.main}>
+
+        </div>
+    )
+}
+export default Main
